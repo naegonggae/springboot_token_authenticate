4d19da832af045f70d9ca1bd2548c8e31fa6e07b
naegonggae <https://github.com/naegonggae/>
2022-12-27 04:26:43 +0900

    TokenFilter에서 권한 모두 부여하기

--- src/main/java/com/token/authenticate/configuration/AuthenticationConfig.java
+++ src/main/java/com/token/authenticate/configuration/AuthenticationConfig.java
@@ -1,5 +1,8 @@
 package com.token.authenticate.configuration;
 
+import com.token.authenticate.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,7 +14,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig {
+
+    private final UserService userService;
+    @Value("${jwt.token.secret}")
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,13 +28,13 @@ public class AuthenticationConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/users/join").permitAll() // join, login은 언제나 가능
+                .antMatchers("/api/v1/users/login").permitAll() // join, login은 언제나 가능
                 .antMatchers(HttpMethod.POST, "/api/v1/reviews").authenticated()  // 문을 만들기 // 모든 post요청을 권한이 있어야 받게함 , 순서도 중요!
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
                 .and()
-                //.addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
+                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
                 // 토큰 필터는 티켓을 산거라고 보면 돼
                 .build();
     }
